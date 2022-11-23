8e03e251d01b5c16beed239b128740d3f549fa84
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-11-23 15:37:40 -0300

    feat: Pré-implementação do componente 'pgm-google-maps' no componente 'MapMarkerComponent'.
    Author: Phelipe Matos (PHMATOS07)

--- apps/poc-google-maps/src/app/map-marker/map-marker.module.ts
+++ apps/poc-google-maps/src/app/map-marker/map-marker.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GoogleMapsModule } from './../../feature-view/google-maps/google-maps.module';
 import { MapMarkerRoutingModule } from './map-marker-routing.module';
 import { MapMarkerComponent } from './map-marker.component';
-
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MapMarkerRoutingModule
+    MapMarkerRoutingModule,
+    GoogleMapsModule
   ],
   exports: [MapMarkerComponent],
   declarations: [MapMarkerComponent]
