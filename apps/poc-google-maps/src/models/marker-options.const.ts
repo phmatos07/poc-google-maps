a9574c9863979c869d6658a6ec3719c1a9cf5238
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-11-25 14:33:18 -0300

    feat: Criação dos arquivos de constantes para sentar posicionamento dos Marcadores e suas configurações.
    Author: Phelipe Matos (PHMATOS07)

--- apps/poc-google-maps/src/models/marker-options.const.ts
+++ apps/poc-google-maps/src/models/marker-options.const.ts
@@ -1,5 +1,5 @@
+
 export const MarkerOptionsConst: google.maps.MarkerOptions = {
-  draggable: true, // Se for `true`, o marcador recebe eventos de mouse e toque.
 
   /**
     * Qual animação reproduzir quando o marcador é adicionado a um mapa.
@@ -7,8 +7,32 @@ export const MarkerOptionsConst: google.maps.MarkerOptions = {
     * DROP (2): O marcador cai do topo do mapa terminando com um pequeno salto.
    */
   animation: 2, // Qual animação reproduzir quando o marcador é adicionado a um mapa.
+
   clickable: true, // Se for `true`, o marcador recebe eventos de mouse e toque.
   crossOnDrag: true, // Se for `false`, desativa a cruz que aparece abaixo do marcador ao arrastar.
   cursor: 'pointer', // Cursor do mouse para mostrar ao passar o mouse.
-  // draggable: true, // Ícone para o primeiro plano. Se uma `string` for fornecida, ela será tratada como embora fosse um {@link Icon} com a `string` como {@link Icon#url url}.
+  draggable: true, // Se for `true`, o marcador recebe eventos de mouse e toque.
+
+  // Ícone para o primeiro plano. Se uma `string` for fornecida, ela será tratada como embora fosse um {@link Icon} com a `string` como {@link Icon#url url}.
+  // icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+  icon: './../../assets/store.svg',
+  // label: MarkerLabel, // Adiciona um rótulo ao marcador. O rótulo pode ser uma `string` ou um MarkerLabel
+  opacity: 1.0, // A opacidade do marcador entre 0,0 e 1,0.
+
+  /**
+    * A otimização renderiza muitos marcadores como um único elemento estático. Otimizado
+    * a renderização é habilitada por padrão. Desative a renderização otimizada para animação
+    * GIFs ou PNGs, ou quando cada marcador deve ser renderizado como um DOM separado
+    * elemento (somente uso avançado).
+  */
+  optimized: true,
+
+  shape: {
+    type: 'rect',
+
+    coords: [10, 20, 30, 10]
+  },
+
+  title: 'Filial/CB: 1000', // Texto de rolagem
+  visible: true, // Se for `true`, o marcador é visível.
 };
--- apps/poc-google-maps/src/models/marker-positions.const.ts
+++ apps/poc-google-maps/src/models/marker-positions.const.ts
@@ -1,14 +1,19 @@
-export const MarkerPositionsConst: google.maps.LatLngLiteral[] = [
+import { MarkerLabel } from "./marker-label.const";
+
+export const MarkerPositionsConst = [
   {
     lat: -23.56482836812248,
-    lng: -46.62904144179103
+    lng: -46.62904144179103,
+    label: MarkerLabel
   },
   {
     lat: -23.533927810358435,
-    lng: -46.61467194562491
+    lng: -46.61467194562491,
+    label: MarkerLabel
   },
   {
     lat: -23.53466655383355,
-    lng: -46.67886464812403
+    lng: -46.67886464812403,
+    label: MarkerLabel
   }
 ];
