e6a6fbb127e4f378a5d09c9e84b9b4adaebc55ce
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-11-26 14:36:37 -0300

    feat: Criação e implementação do 'MarkerLabelBuilder'.
    Author: Phelipe Matos (PHMATOS07)

--- apps/poc-google-maps/src/models/marker-label.const.ts
+++ apps/poc-google-maps/src/models/marker-label.const.ts
@@ -1,9 +1,9 @@
-export const MarkerLabel: google.maps.MarkerLabel = {
+export const MarkerLabel: Partial<google.maps.MarkerLabel> = {
 
   /**
    * A propriedade className do elemento do rótulo (equivalente ao atributo class do elemento).
     * Várias classes CSS separadas por espaço podem ser adicionadas. O padrão é nenhuma classe CSS (uma string vazia).
-    * A cor, o tamanho, o peso e a família da fonte só podem ser definidos por meio das outras propriedades de {@link MarkerLabel}.
+    * A cor, o tamanho, o peso e a família da fonte só podem ser definidos por meio das outras propriedades de MarkerLabel.
     * As classes CSS não devem ser usadas para alterar a posição nem
     * orientação do rótulo (por exemplo, usando translações e rotações) se também estiver usando
    */
@@ -12,5 +12,5 @@ export const MarkerLabel: google.maps.MarkerLabel = {
   // fontFamily: 'string', // A família de fontes do texto do rótulo.
   // fontSize: '16px', // Tamanho da fonte de texto
   // fontWeight: 'bold', //O peso da fonte do texto do rótulo.
-  text: 'Filial/CB: 1000' //O texto a ser exibido no rótulo.
+  // text: 'Filial/CB: 1000' //O texto a ser exibido no rótulo.
 };
--- apps/poc-google-maps/src/models/marker-positions.const.ts
+++ apps/poc-google-maps/src/models/marker-positions.const.ts
@@ -1,19 +1,26 @@
-import { MarkerLabel } from "./marker-label.const";
+import { MarkerLabelBuilder } from "./marker-label.builder";
+import { MarkerOptionsConst } from "./marker-options.const";
 
 export const MarkerPositionsConst = [
   {
     lat: -23.56482836812248,
     lng: -46.62904144179103,
-    label: MarkerLabel
+    label: new MarkerLabelBuilder('Filial/CB: 1000').markerLabel,
+    title: 'Filial/CB: 1000',
+    options: MarkerOptionsConst
   },
   {
     lat: -23.533927810358435,
     lng: -46.61467194562491,
-    label: MarkerLabel
+    label: new MarkerLabelBuilder('Filial/CB: 2000').markerLabel,
+    title: 'Filial/CB: 2000',
+    options: MarkerOptionsConst
   },
   {
     lat: -23.53466655383355,
     lng: -46.67886464812403,
-    label: MarkerLabel
+    label: new MarkerLabelBuilder('Filial/CB: 3000').markerLabel,
+    title: 'Filial/CB: 3000',
+    options: MarkerOptionsConst
   }
 ];
