a56d421c1616a669dd5907d7e171d516094ae5a3
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-11-28 16:49:44 -0300

    feat: Criação e implementação da interface 'MarkerPositions'.
    Author: Phelipe Matos (PHMATOS07)

--- apps/poc-google-maps/src/models/marker-positions.const.ts
+++ apps/poc-google-maps/src/models/marker-positions.const.ts
@@ -1,26 +1,26 @@
-import { MarkerLabelBuilder } from "./marker-label.builder";
-import { MarkerOptionsConst } from "./marker-options.const";
+import { MarkerOptionsBuilder } from "./marker-options.builder";
+import { MarkerPositions } from "./marker-positions";
 
-export const MarkerPositionsConst = [
+export const MarkerPositionsConst: MarkerPositions[] = [
   {
     lat: -23.56482836812248,
     lng: -46.62904144179103,
-    label: new MarkerLabelBuilder('Filial/CB: 1000').markerLabel,
-    title: 'Filial/CB: 1000',
-    options: MarkerOptionsConst
+    // label: new MarkerLabelBuilder('Filial: 1000').markerLabel,
+    // title: 'Filial: 1000',
+    options: new MarkerOptionsBuilder().markerOptions
   },
   {
     lat: -23.533927810358435,
     lng: -46.61467194562491,
-    label: new MarkerLabelBuilder('Filial/CB: 2000').markerLabel,
-    title: 'Filial/CB: 2000',
-    options: MarkerOptionsConst
+    // label: new MarkerLabelBuilder('Filial: 2000').markerLabel,
+    // title: 'Filial: 2000',
+    options: new MarkerOptionsBuilder().markerOptions
   },
   {
     lat: -23.53466655383355,
     lng: -46.67886464812403,
-    label: new MarkerLabelBuilder('Filial/CB: 3000').markerLabel,
-    title: 'Filial/CB: 3000',
-    options: MarkerOptionsConst
+    // label: new MarkerLabelBuilder('Filial: 3000').markerLabel,
+    // title: 'Filial: 3000',
+    options: new MarkerOptionsBuilder().markerOptions
   }
 ];
---
+++ apps/poc-google-maps/src/models/marker-positions.ts
@@ -0,0 +1,7 @@
+export interface MarkerPositions {
+  lat: number;
+  lng: number;
+  label?: google.maps.MarkerLabel;
+  title?: string;
+  options?: google.maps.MarkerOptions;
+}
