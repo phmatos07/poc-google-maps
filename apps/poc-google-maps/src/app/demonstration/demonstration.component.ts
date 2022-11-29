20675a321c4b8e459e502c4bd31c710685cb07ab
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-11-29 19:52:04 -0300

    feat: Implementação do formulário reativo e diagramação do layout, usando o componente do Material Form.
    Author: Phelipe Matos (PHMATOS07)

--- apps/poc-google-maps/src/app/demonstration/demonstration.component.ts
+++ apps/poc-google-maps/src/app/demonstration/demonstration.component.ts
@@ -1,13 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { GoogleMapsOptionsConst } from './../../models/google-maps-options.const';
 
 @Component({
   selector: 'pgm-demonstration',
   templateUrl: './demonstration.component.html',
   styleUrls: ['./demonstration.component.scss']
 })
-export class DemonstrationComponent implements OnInit {
+export class DemonstrationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  formMapsConfig?: AbstractControl;
+  private subscriptionForm?: Subscription;
 
-  ngOnInit(): void { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptionForm) {
+      this.subscriptionForm.unsubscribe();
+    }
+  }
+
+  private createForm(): void {
+    this.formMapsConfig = this.formBuilder.group({
+      width: [GoogleMapsOptionsConst.width],
+      height: [GoogleMapsOptionsConst.height],
+      lat: [GoogleMapsOptionsConst.center.lat],
+      lng: [GoogleMapsOptionsConst.center.lng],
+      zoom: [GoogleMapsOptionsConst.zoom],
+    });
+    this.subscriptionForm = this.getFormData();
+  }
+
+  private getFormData(): Subscription {
+    return this.formMapsConfig.valueChanges
+      .subscribe(value => console.log(value));
+  }
 }
--- apps/poc-google-maps/src/app/demonstration/demonstration.module.ts
+++ apps/poc-google-maps/src/app/demonstration/demonstration.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule as GoogleMapsModuleNative } from '@angular/google-maps';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { DemonstrationRoutingModule } from './demonstration-routing.module';
 import { DemonstrationComponent } from './demonstration.component';
@@ -9,8 +12,11 @@ import { DemonstrationComponent } from './demonstration.component';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
     GoogleMapsModuleNative,
-    DemonstrationRoutingModule
+    DemonstrationRoutingModule,
   ],
   exports: [DemonstrationComponent],
   declarations: [DemonstrationComponent]
