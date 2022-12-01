8adf7e16a62fe5f7b6714022dc0c52b2eb2e8c18
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2022-12-01 15:20:33 -0300

    feat: Pré-implementação do mapTypeId, para mudar o tipo de visualização do Mapa.

--- apps/poc-google-maps/src/models/google-maps-options.const.ts
+++ apps/poc-google-maps/src/models/google-maps-options.const.ts
@@ -1,8 +1,10 @@
 import { GoogleMapsOptions } from "./google-maps-options";
+import { MapTypeId } from "./map-typeId.enum";
 
 export const GoogleMapsOptionsConst: Required<GoogleMapsOptions> = {
   width: "100%",
   height: "480px",
   center: { lat: -23.55548454802656, lng: -46.640139324889695 },
-  zoom: 13
+  zoom: 13,
+  mapTypeId: MapTypeId.ROADMAP,
 };
